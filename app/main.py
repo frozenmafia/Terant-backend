4c8ce3cfab49ca31ad5a11c3253c3290a4455c55
anchalshivank <anchalshivank99@gmail.com>
2024-06-08 02:28:55 +0530

    created classes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, File, Body, Request
+import json
+from typing import Annotated, List, Optional
 from fastapi.middleware.cors import CORSMiddleware  # Import CORSMiddleware
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from . import models, database
+import logging
+import re
 app = FastAPI()
 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
@@ -30,6 +37,18 @@ class SensorDataCreate(BaseModel):
             f"PF={self.PF})"
         )
 
+
+@app.middleware("http")
+async def log_request(request, call_next):
+    # logger.info(f"Incoming request: {request.method} {request.url}")
+    # logger.info(f"Request headers: {request.headers}")
+    # logger.info(f"Request body: {await request.body()}")
+    # logger.info(f"Request :{request}")
+
+    response = await call_next(request)
+
+    return response
+
 @app.get("/")
 def get_home():
     return {"message": "Welcome to the homepage!"}
@@ -44,27 +63,96 @@ def validate_sensor_data(sensor_data: SensorDataCreate) -> bool:
         ]
     )
 
+# @app.post("/receive_data")
+# async def receive_data(data):
+#   """
+#   This endpoint receives data containing timestamps from an ESP32 device.
+#   """
+#   # Split the data into lines and strip any leading/trailing whitespaces
+#   lines = data.decode().strip().splitlines()
+#   measurements = []
+#   for line in lines:
+#     # Extract the time value from each line
+#     time_value = int(line.split(":")[1].strip())
+#     # Create a Measurement object and append it to the list
+#     measurements.append(Measurement(time=time_value))
+  
+#   # Process the list of measurements (logic depends on your specific needs)
+#   # You can store them in a database, send them for further processing, etc.
+#   # ... (your processing logic) ...
+  
+#   return {"message": "Data received successfully!"}
+
+# @app.post("/receive_data")
+# async def receive_data(sensor_data:Annotated[bytes, File()], db: Session = Depends(database.get_db)):
+#     if not validate_sensor_data(sensor_data):
+#         raise HTTPException(status_code=400, detail="Received corrupted data")
+    
+
+#     # new_data = models.SensorData(
+#     #     voltage=sensor_data.Voltage,
+#     #     current=sensor_data.Current,
+#     #     power=sensor_data.Power,
+#     #     energy=sensor_data.Energy,
+#     #     frequency=sensor_data.Frequency,
+#     #     pf=sensor_data.PF
+#     # )
+#     # print(sensor_data)
+#     # db.add(new_data)
+#     # db.commit()
+#     # db.refresh(new_data)
+#     print(sensor_data)
+    
+#     return {"message": "Data received successfully"}
+
+class Reading(BaseModel):
+    module: int
+    voltage: Optional[float]
+    current: Optional[float]
+    power: Optional[float]
+    energy: Optional[float]
+    frequency: Optional[float]
+    powerFactor: Optional[float]
+
+    def to_json(self) -> str:
+        return json.dumps(self.dict())
+
+class ReadingData(BaseModel):
+    timestamp: int
+    readings: List[Reading]
+
+    def to_json(self) -> str:
+        return json.dumps(self.dict())
+
+class SensorData(BaseModel):
+    deviceId: str
+    data: List[ReadingData]
+
+    def to_json(self) -> str:
+        return json.dumps(self.dict())
 @app.post("/receive_data")
-async def receive_data(sensor_data: SensorDataCreate, db: Session = Depends(database.get_db)):
-    if not validate_sensor_data(sensor_data):
-        raise HTTPException(status_code=400, detail="Received corrupted data")
-    
+async def receive_data(request: Request):
+    try:
+        # Read the request body as bytes
+        body_bytes = await request.body()
+        
+        # Decode bytes to string
+        body_str = body_bytes.decode("utf-8")
+        # print(body_str)
+        json_body = json.loads(body_str)
+        # print(json_body)
+        sensor_data = SensorData(**json_body)
+        print(sensor_data.to_json())
 
-    new_data = models.SensorData(
-        voltage=sensor_data.Voltage,
-        current=sensor_data.Current,
-        power=sensor_data.Power,
-        energy=sensor_data.Energy,
-        frequency=sensor_data.Frequency,
-        pf=sensor_data.PF
-    )
-    print(sensor_data)
-    db.add(new_data)
-    db.commit()
-    db.refresh(new_data)
-    
+
+    except json.JSONDecodeError:
+        print("Invalid JSON")
+        raise HTTPException(status_code=422, detail="Invalid JSON")
+    except Exception as e:
+        print("Validation error")
+        raise HTTPException(status_code=422, detail=f"Validation error: {e}")
+
     return {"message": "Data received successfully"}
-
 
 @app.get("/readings")
 async def readings(db:Session = Depends(database.get_db)):
